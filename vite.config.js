50f968c7cb09dfca3373c981a5e598b58ea291c9
LaCovLoid <tnsgus0018@naver.com>
2026-01-04 05:11:21 +0900

    프록시 우회를 위한 수정

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      "/gas": {
+        target: "https://script.google.com",
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/gas/, ""),
+      },
     },
   },
 });
